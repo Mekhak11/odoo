d85d479caf5038f4a487e1bb5b26ddc6f3ce960b
Mekhak <mekhakghapantsyan@gmail.com>
2025-05-25 00:10:21 +0400

    Initial Commit

---
+++ odoo/odooApp.swift
@@ -0,0 +1,17 @@
+//
+//  odooApp.swift
+//  odoo
+//
+//  Created by Mekhak Ghapantsyan on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct odooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
